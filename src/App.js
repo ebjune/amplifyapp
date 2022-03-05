33bb2ababe534e18cd63c221b095455031e1ff75
Erika Bjune <erika@simmunity.com>
2022-03-05 12:34:23 -0800

    Updated authenticator to latest

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+// <AmplifySignOut />
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+const Home = () => {
+    const { user, signOut } = useAuthenticator((context) => [context.user]);
+
+    return (
+        <>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>We now have Auth!</h1>
+                </header>
+            </div>
+            <h2>Welcome, {user.username}!</h2>
+            <button onClick={signOut}>Sign Out</button>
+        </>
+    );
+};
+
+const Login = () => <Authenticator />;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
-    </div>
-  );
+    const { route } = useAuthenticator((context) => [context.route]);
+    return route === 'authenticated' ? <Home /> : <Login />;
 }
+// export default App;
+// export default withAuthenticator(App);
+/* return (
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>We now have Auth!</h1>
+        </header>
+    </div>;
+); */
 
-export default withAuthenticator(App);
+export default function AppWithProvider() {
+    return (
+        <Authenticator.Provider>
+            <App></App>
+        </Authenticator.Provider>
+    );
+}
