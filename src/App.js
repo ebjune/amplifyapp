c03ea341a175c4fac15d1a4d5d1732364e77063e
Erika Bjune <erika@simmunity.com>
2022-03-05 14:33:37 -0800

    Added GraphQL data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { API } from 'aws-amplify';
 // import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 // <AmplifySignOut />
 import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
 const Home = () => {
     const { user, signOut } = useAuthenticator((context) => [context.user]);
@@ -25,9 +28,71 @@ const Home = () => {
 
 const Login = () => <Authenticator />;
 
+const initialFormState = { name: '', description: '' }
+
 function App() {
     const { route } = useAuthenticator((context) => [context.route]);
-    return route === 'authenticated' ? <Home /> : <Login />;
+
+    // Add GraphQL Data
+    const [notes, setNotes] = useState([]);
+    const [formData, setFormData] = useState(initialFormState);
+
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+    async function fetchNotes() {
+        const apiData = await API.graphql({ query: listNotes });
+        setNotes(apiData.data.listNotes.items);
+    }
+
+    async function createNote() {
+        if (!formData.name || !formData.description) return;
+        await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+        setNotes([ ...notes, formData ]);
+        setFormData(initialFormState);
+    }
+
+    async function deleteNote({ id }) {
+        const newNotesArray = notes.filter(note => note.id !== id);
+        setNotes(newNotesArray);
+        await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+    }
+    // End GraphQL Data
+
+    // return route === 'authenticated' ? <Home /> : <Login />;
+    if (route === 'authenticated'){
+        return (
+            <div className="App">
+                <h1>My Notes App</h1>
+                <input
+                    onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+                    placeholder="Note name"
+                    value={formData.name}
+                />
+                <input
+                    onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+                    placeholder="Note description"
+                    value={formData.description}
+                />
+                <button onClick={createNote}>Create Note</button>
+                <div style={{marginBottom: 30}}>
+                    {
+                        notes.map(note => (
+                            <div key={note.id || note.name}>
+                                <h2>{note.name}</h2>
+                                <p>{note.description}</p>
+                                <button onClick={() => deleteNote(note)}>Delete note</button>
+                            </div>
+                        ))
+                    }
+                </div>
+                <Home />
+            </div>
+        )
+    } else {
+        return <Login />
+    }
 }
 // export default App;
 // export default withAuthenticator(App);
